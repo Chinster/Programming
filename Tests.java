0090c0ab381f2fdc9ecfcd1e537da9788bc45256
Chris Chin <ctchin13@gmail.com>
2014-01-14 22:27:21 -0500

    Updated problem9. Must go deeper.

---
+++ Tests.java
@@ -0,0 +1,30 @@
+/**
+ * Tests.java.
+ *
+ * @author Christopher Chin
+ *
+ */
+public class Tests
+{
+    
+    /**
+     * Program.
+     *
+     */
+    public Tests()
+    {
+        int i = 200;
+        System.out.println( Math.pow( i, 2 ) );
+
+    }
+    
+    /**
+     * MAIN.
+     * 
+     * @param args String[]
+     */
+    public static void main( String[] args )
+    {
+        new Tests();
+    }
+}
