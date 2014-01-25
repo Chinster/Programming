2f59cee0840aff8b963a5a299bbb72047c20d15d
Chris Chin <ctchin13@gmail.com>
2014-01-25 01:20:19 -0500

    Completed problem12 in java

--- problem012/problem12.java
+++ problem012/problem12.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
  * ProjectEuler - Problem 12 Solution
  * problem12.java.
@@ -10,7 +11,7 @@ public class problem12
     
     /**
      * First triangle number to have over 500 divisors.
-     *
+     * Divisor =
      * Triangle number = sum of sequence of natural numbers;
      * 1: 1
      * 3: 1+2
@@ -19,10 +20,40 @@ public class problem12
      */
     public problem12()
     {
-
-
+        boolean notFound = true;
+        int i = 1;
+        int triangleNum = 0;
+        while(notFound)
+        {
+            triangleNum += i;
+            System.out.println( triangleNum );
+            if( getDivisors( triangleNum ).size() > 500 )
+                notFound = false;
+            i++;
+        }
+        System.out.println( triangleNum );
     }
-    
+    /**
+     * Get divisors.
+     *
+     * @param double num
+     * @return ArrayList<Integer> divisors
+     */
+    public static ArrayList<Integer> getDivisors( int num )
+    {
+        ArrayList<Integer> returnArray = new ArrayList<Integer>();
+        returnArray.add( new Integer( 1 ) );
+        for( int i = 2; i <= num/i; i++ )
+        {
+            if( num%i == 0)
+            {
+                returnArray.add( new Integer( i ));
+                returnArray.add( new Integer( (int)(num/i)));
+            }
+        }
+        returnArray.add( new Integer((int)num) );
+        return returnArray;
+    }
     /**
      * MAIN.
      * 
