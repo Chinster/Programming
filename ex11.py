210327a0cc18fee82eae5e620ebccb1423a0091a
Christopher Chin <ctchin13@gmail.com>
2014-03-31 16:50:07 -0400

    Reading input, reading files, functions

---
+++ ex11.py
@@ -0,0 +1,18 @@
+# Exercise 11: Asking Questions
+
+# python 2 input
+# print( "How old are you?", )
+# age = raw_input() 
+age = input( "How old are you? " )
+
+# python 2 input
+# print( "How tall are you?", )
+# height = raw_input()
+height = input( "How tall are you? " )
+
+# python 2 input
+# print( "How much do you weigh?", )
+# weight = raw_input()
+weight = input( "How much do you weigh? " )
+
+print( "So, you're %r old, %r tall, and %r heavy." % (age, height, weight) )
---
+++ ex16.py
@@ -0,0 +1,37 @@
+# Exercise 16: Reading and Writing Files
+
+from sys import argv
+
+script, filename = argv
+
+print( "We're going to erase %r." % filename )
+print( "If you don't want that, hit CTRL-C (^C)." )
+print( "If you do want that, hit RETURN.")
+
+input( "?" )
+
+print( "Opening the file..." )
+target = open( filename, 'w' )
+
+print( "Truncating the file. Goodbye!" )
+# Not necessary, write mode truncates the file
+# a for appending mode adds to end of file.
+target.truncate()
+
+print( "Now I'm going to ask you for three lins." )
+
+line1 = input( "Line 1: ")
+line2 = input( "Line 2: ") 
+line3 = input( "Line 3: ")
+
+print( "I'm going to write these to the file." )
+
+target.write( line1 )
+target.write( "\n" )
+target.write( line2 )
+target.write( "\n" )
+target.write( line3 )
+target.write( "\n" )
+
+print( "And finally, we close it." )
+target.close()
