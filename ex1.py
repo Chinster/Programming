518ecb9158541b7b1990b2606243fe87da091c79
Christopher Chin <ctchin13@gmail.com>
2014-03-31 08:56:03 -0400

    hardway-python init

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# print is a function in python 3
+# print() works in python 2 though
+
+print ("Hello world!")
+print ('Salvete')
+print ('Such python. "Much" syntax.')
+print ("Very whitespace. 'Wow'.")
+#print ("Very commented.")
---
+++ ex2.py
@@ -0,0 +1,5 @@
+# Just like a bash script up in here.
+# Hash/Pounds/Octothorpes are comments.
+print( "Comments are great." ) # Should break the whitespace habit
+print( "# are comments in python." )
+
