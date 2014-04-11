247ed2a5d2040669069529f59a82532bc1f324ad
Christopher Chin <ctchin13@gmail.com>
2014-04-10 21:51:55 -0400

    Starting ex45

---
+++ learn-hard-way/ex45/main.py
@@ -0,0 +1,22 @@
+class Character(object):
+    def __init__(self):
+        pass
+    def hurt(int):
+        
+class Hero(Character):
+    def __init__(self):
+        pass
+class Enemy(Character):
+    def __init__(self):
+        pass
+class Weapon(object):
+    def __init__(self, attack, speed):
+        power = attack
+class Scene(object):
+    def __init__(self, descript):
+        pass
+class Battle(Scene):
+    def __init__(self, descript):
+        pass
+class Zone(Scene):
+
