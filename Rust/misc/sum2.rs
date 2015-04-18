c348b8adf8f206ee210834b7d198d4a18177d86b
Chris Chin <ctchin13@gmail.com>
2015-04-18 19:21:42 -0400

    Rust stdio work

--- Rust/misc/sum2.rs
+++ Rust/misc/sum2.rs
@@ -1,12 +1,13 @@
-///Takes two numbers from stdin and prints their sum to stdout
+///Takes two numbers seperately from stdin and prints their sum to stdout
 fn main() {
     let mut stdin = std::io::stdin();
     let mut sum = 0;
+
     let mut line = String::new();
     match stdin.read_line(&mut line) {
         Ok(_) => match line.trim().parse::<u32>() {
                 Ok(num) => sum += num,
-                Err(_) => println!("Error due to: {}", reason),
+                Err(_) => println!("Not a number"),
             },
         Err(_) => println!("Could not read line."),
     }
@@ -15,9 +16,9 @@ fn main() {
     match stdin.read_line(&mut line) {
         Ok(_) => match line.trim().parse::<u32>() {
                 Ok(num) => sum += num,
-                Err(reason) => println!("Error due to: {}", reason),
+                Err(_) => println!("Not a number"),
             },
-        Err(reason) => println!("Error: {}", reason),
+        Err(_) => println!("Could not read line."),
     }
     println!("{}", sum);
 }
