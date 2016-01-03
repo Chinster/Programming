5251c1af0f8421d03b8f0fbe9ee05efbc0d417a3
Christopher Chin <ctchin13@gmail.com>
2016-01-03 13:36:42 -0500

    C hard way, Ex 26.

---
+++ C/hard-way/ex26/shell.h
@@ -0,0 +1,36 @@
+/* An interface for running shell commands
+ */
+
+#ifndef _SHELL_H
+#define _SHELL_H
+
+#define MAX_COMMAND_ARGS 100
+
+#include <apr_thread_proc.h>
+
+// Stores all command information
+typedef struct Shell {
+    const char *dir;
+    const char *exe;
+    int args_cnt;
+
+    apr_procattr_t *attr;
+    apr_proc_t proc;
+    apr_exit_why_e exit_why;
+    int exit_code;
+
+    const char *args[MAX_COMMAND_ARGS];
+} Shell;
+
+int Shell_run(apr_pool_t *p, Shell *cmd);
+int Shell_exec(Shell cmd, ...);
+
+extern Shell CLEANUP_SH;
+extern Shell GIT_SH;
+extern Shell TAR_SH;
+extern Shell CURL_SH;
+extern Shell CONFIGURE_SH;
+extern Shell MAKE_SH;
+extern Shell INSTALL_SH;
+
+#endif
