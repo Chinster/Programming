72ce0c71c07cd1cbb0996b900ebf5096b851aae5
Christopher Chin <ctchin13@gmail.com>
2016-05-28 19:49:52 -0400

    Install yaourt

--- Scripts/arch-install.sh
+++ Scripts/arch-install.sh
@@ -6,8 +6,10 @@
 #  -Somehow we must link the dependencies to the AUR package in pacman's database.
 
 ARCH_TARGET_DIR=$2
-OTHER_PKGS="git vim wpa_supplicant alsa-utils"
+OTHER_PKGS="git vim wpa_supplicant alsa-utils wget"
 EXTRA_PKGS="sway rxvt-unicode firefox"
+PACKAGE_QUERY_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz"
+YAOURT_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz"
 
 # Check for root
 if [[ $EUID -ne 0 ]]; then
@@ -54,6 +56,32 @@ pacman -S $OTHER_PKGS --noconfirm
 pacman -S $EXTRA_PKGS --noconfirm
 exit
 " > $ARCH_TARGET_DIR/chroot-script.sh
+
+# Create an environment to allow the use of makepkg without non-root users.
+# Install package-query from AUR and yaourt. yajl is a dependency of package-query
+echo "pacman -S yajl --noconfirm --asdeps
+mkdir /home/build
+chgrp nobody /home/build
+chmod g+ws /home/build
+setfacl -m u::rwx,g::rwx /home/build
+setfacl -d --set u::rwx,g::rwx,o::- /home/build
+cd /home/build
+sudo -u nobody wget $PACKAGE_QUERY_URL
+sudo -u nobody wget $YAOURT_URL
+sudo -u nobody tar -xf package-query.tar.gz
+sudo -u nobody tar -xf yaourt.tar.gz
+cd package-query
+sudo -u nobody makepkg
+pacman -U package-query*.pkg.*
+cd ../yaourt
+sudo -u nobody makepkg
+pacman -U yaourt*.pkg.*
+
+rm -rf /home/build
+" > $ARCH_TARGET_DIR/chroot-script.sh
+
+# Done with in-chroot script
+echo "exit" > $ARCH_TARGET_DIR/chroot-script.sh
 
 chmod a+x $ARCH_TARGET_DIR/chroot-script.sh
 
