9f94c796987f716bce338a578584334ad4cadff8
Christopher Chin <ctchin13@gmail.com>
2016-12-10 23:02:13 -0500

    hi glium

--- Scripts/mount-sysfs.sh
+++ Scripts/mount-sysfs.sh
@@ -1,4 +1,11 @@
-DEST=/Downloads/gentoo
+DEST=$1
+
+if [ ! $# == 1 ];
+then
+    echo "Usage: mount-sysfs CHROOT"
+    exit 1
+fi
+
 sudo mount -t proc proc $DEST/proc/
 sudo mount --rbind /dev $DEST/dev/
 sudo mount --rbind /sys $DEST/sys/
