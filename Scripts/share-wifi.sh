8c83b00c1eb3336317042ee31ed3b99afd99053f
Christopher Chin <ctchin13@gmail.com>
2017-07-20 21:06:31 -0400

    Fix script.

--- Scripts/share-wifi.sh
+++ Scripts/share-wifi.sh
@@ -7,6 +7,6 @@ WLAN=wlp2s0
 #ip a a dev $ETH 192.168.0.14/24
 
 sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $WLAN -j MASQUERADE
 
 ip l s $ETH up
